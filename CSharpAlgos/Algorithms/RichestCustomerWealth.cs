6664228394b7528329d5808f135550fea8648de6
Nico Muszalczyk <74101214+nico-mu@users.noreply.github.com>
2022-01-31 14:56:31 +0100

    adding richest customer wealth

---
+++ CSharpAlgos/Algorithms/RichestCustomerWealth.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAlgos.Algorithms
+{
+    internal class RichestCustomerWealth
+    {
+        public static int MaximumWealth(int[][] accounts)
+        {
+            int richest = 0;
+            foreach (var account in accounts)
+            {
+                richest = Math.Max(richest, account.Sum());
+            }
+            return richest;
+        }
+    }
+}
