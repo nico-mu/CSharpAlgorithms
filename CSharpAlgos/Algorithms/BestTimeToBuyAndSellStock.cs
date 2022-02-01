a9a97b7e9ce24faa6d343e85284c3e7c53f842a0
Nico Muszalczyk <74101214+nico-mu@users.noreply.github.com>
2022-02-01 11:41:06 +0100

    adding best time to buy and sell stock

---
+++ CSharpAlgos/Algorithms/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAlgos.Algorithms
+{
+    internal class BestTimeToBuyAndSellStock
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            int min = int.MaxValue, maxProfit = 0;
+            foreach (var price in prices)
+            {
+                min = Math.Min(min, price);
+                maxProfit = Math.Max(maxProfit, price - min);
+            }
+            return maxProfit;
+        }
+    }
+}
--- CSharpAlgos/Program.cs
+++ CSharpAlgos/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CSharpAlgos.Algorithms;
 
-Console.WriteLine(LargestRectangleInHistogram.LargestRectangleArea(new int[] { 5, 5, 1, 7, 1, 1, 5, 2, 7, 6 }));
+Console.WriteLine(BestTimeToBuyAndSellStock.MaxProfit(new int[] { 7, 6, 4, 3, 1 }));
