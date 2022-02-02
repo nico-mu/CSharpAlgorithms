ee7789c2479889eaa3f7afe6631613aba8f7747c
Nico Muszalczyk <74101214+nico-mu@users.noreply.github.com>
2022-02-02 18:02:07 +0100

    Adding FirstBadItem

---
+++ CSharpAlgos/Leetcode/FirstBadItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAlgos.Leetcode
+{
+    internal class FirstBadItem
+    {
+        public static int FirstBadVersion(int n)
+        {
+            if (n < 2)
+            {
+                return n;
+            }
+            return binarySearch(0, n);
+        }
+        private static int binarySearch(int leftPointer, int rightPointer)
+        {
+            if (leftPointer > rightPointer)
+            {
+                return leftPointer;
+            }
+            int mid = leftPointer + (rightPointer - leftPointer) / 2;
+            if (mid != 1)
+            {
+                return binarySearch(mid + 1, rightPointer);
+            }
+            return binarySearch(leftPointer, mid - 1);
+        }
+    }
+}
--- CSharpAlgos/Program.cs
+++ CSharpAlgos/Program.cs
@@ -1,5 +1,5 @@
 ﻿using CSharpAlgos.Algorithms;
 using CSharpAlgos.Leetcode;
 
-Console.WriteLine(CSharpAlgos.Algorithms.BinarySearch.Search(new int[] { -1, 0, 3, 5, 9, 12 }, 2));
+Console.WriteLine(CSharpAlgos.Leetcode.FirstBadItem.FirstBadVersion(2));
 
