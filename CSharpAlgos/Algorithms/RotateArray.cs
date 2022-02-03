637f73e5b7b1eb034176f5e7422fd510e41313e8
Nico Muszalczyk <74101214+nico-mu@users.noreply.github.com>
2022-02-03 11:19:16 +0100

    adding rotate array

---
+++ CSharpAlgos/Algorithms/RotateArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAlgos.Leetcode
+{
+    internal class RotateArray
+    {
+        public static void Rotate(int[] nums, int k)
+        {
+            if (nums == null || nums.Length <= 1)
+            {
+                return;
+            }
+            int n = nums.Length;
+            k = k % n;
+            Reverse(nums, 0, n - 1);
+            Reverse(nums, 0, k - 1);
+            Reverse(nums, k, n - 1);
+        }
+
+        public static void Reverse(int[] arr, int start, int end)
+        {
+            int temp;
+            while (start < end)
+            {
+                temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
