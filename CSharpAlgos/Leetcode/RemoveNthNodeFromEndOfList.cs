cbdd4028f90dcf154d20cc838d53b26f0460e880
Nico Muszalczyk <74101214+nico-mu@users.noreply.github.com>
2022-02-06 10:10:08 +0100

    adding remove node from end of list

---
+++ CSharpAlgos/Leetcode/RemoveNthNodeFromEndOfList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAlgos.Leetcode
+{
+    internal class RemoveNthNodeFromEndOfList
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            ListNode result = new ListNode(-1);
+            result.next = head;
+            ListNode leftPointer = result, rightPointer = result;
+            while (n > 0)
+            {
+                rightPointer = rightPointer.next;
+                n--;
+            }
+            while (rightPointer.next != null)
+            {
+                leftPointer = leftPointer.next;
+                rightPointer = rightPointer.next;
+            }
+            leftPointer.next = leftPointer.next.next;
+            return result.next;
+        }
+    }
+}
