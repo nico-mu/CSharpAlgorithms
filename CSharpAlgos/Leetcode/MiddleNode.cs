79d30ef15605740f8b2fec8d97c74675d127e9cd
Nico Muszalczyk <74101214+nico-mu@users.noreply.github.com>
2022-02-06 09:43:44 +0100

    adding MiddleNode

---
+++ CSharpAlgos/Leetcode/MiddleNode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAlgos.Leetcode
+{
+    class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    internal class MiddleNode
+    {
+        public static ListNode SolveMiddleNode(ListNode head)
+        {
+            ListNode leftPointer = head;
+            ListNode rightPointer = head;
+            while (rightPointer != null && rightPointer.next != null)
+            {
+                leftPointer = leftPointer.next;
+                rightPointer = rightPointer.next.next;
+            }
+            return leftPointer;
+        }
+    }
+}
