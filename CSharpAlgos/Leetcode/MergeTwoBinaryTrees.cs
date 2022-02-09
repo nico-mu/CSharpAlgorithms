3da90608b0fa947062f13239e2a89518405e48ca
Nico Muszalczyk <74101214+nico-mu@users.noreply.github.com>
2022-02-09 11:32:20 +0100

    adding merge two binary trees

---
+++ CSharpAlgos/Leetcode/MergeTwoBinaryTrees.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAlgos.Leetcode
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    internal class MergeTwoBinaryTrees
+    {
+        public TreeNode MergeTrees(TreeNode root1, TreeNode root2)
+        {
+            if (root1 == null && root2 == null) return null;
+            int val = (root1 == null ? 0 : root1.val) + (root2 == null ? 0 : root2.val);
+            TreeNode root = new TreeNode(val);
+            root.left = MergeTrees(root1?.left, root2?.left);
+            root.right = MergeTrees(root1?.right, root2?.right);
+            return root;
+        }
+    }
+}
