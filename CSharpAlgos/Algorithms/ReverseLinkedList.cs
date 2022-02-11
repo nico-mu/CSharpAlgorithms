bf27be6bd4b25227ad3bf0a4b7cb37ddb02ff2b8
Nico Muszalczyk <74101214+nico-mu@users.noreply.github.com>
2022-02-11 10:30:24 +0100

    adding reverse linked list

---
+++ CSharpAlgos/Algorithms/ReverseLinkedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAlgos.Algorithms
+{
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    internal class ReverseLinkedList
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null) return head;
+            return ReverseListHelper(null, head);
+        }
+
+        private ListNode ReverseListHelper(ListNode prev, ListNode curr)
+        {
+            if (curr == null) return prev;
+            ListNode next = curr.next;
+            curr.next = prev;
+            return ReverseListHelper(curr, next);
+        }
+    }
+}
