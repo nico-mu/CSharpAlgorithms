d1d226432510db778751211432951f56cab04633
Nico Muszalczyk <74101214+nico-mu@users.noreply.github.com>
2022-02-11 10:12:58 +0100

    adding two sorted lists

---
+++ CSharpAlgos/Leetcode/MergeTwoSortedLists.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAlgos.Leetcode
+{
+    internal class MergeTwoSortedLists
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            if (list1 == null)
+            {
+                return list2;
+            }
+            if (list2 == null)
+            {
+                return list1;
+            }
+            if (list1.val <= list2.val)
+            {
+                list1.next = MergeTwoLists(list1.next, list2);
+                return list1;
+            }
+            else
+            {
+                list2.next = MergeTwoLists(list1, list2.next);
+                return list2;
+            }
+        }
+    }
+}
